4ac3d4b8c3894f5f44dcde5b996ae37f6ec0b70b
chenglongZHENG <zhengtsp17@gmail.com>
2020-08-17 10:02:37 +0200

    make chrome runnable as root user2

--- client/request_client.py
+++ client/request_client.py
@@ -43,8 +43,8 @@ def simulation_youtube(hostip, service_port, ID, generator_zipf):
     generator_zipf = generator_zipf
     opts = Options()
     opts.set_headless()
-    options.addArguments("--no-sandbox")
-    options.addArguments("--disable-dev-shm-usage")
+    opts.add_argument("--no-sandbox")
+    opts.add_argument("--disable-dev-shm-usage")
     # Operating in headless mode
     assert opts.headless is True, "headless has not been set yet"
     browser = Chrome(options=opts)
@@ -69,12 +69,13 @@ def simulation_youtube(hostip, service_port, ID, generator_zipf):
 def simulation_netflix(hostip, service_port, ID, generator_zipf):
     generator_zipf = generator_zipf
     opts = Options()
-    options.addArguments("--no-sandbox")
-    options.addArguments("--disable-dev-shm-usage")
+    opts.add_argument("--no-sandbox")
+    opts.add_argument("--disable-dev-shm-usage")
     opts.set_headless()
     # Operating in headless mode
     assert opts.headless is True, "headless has not been set yet"
     browser = Chrome(options=opts)
+    
 
     hostip = hostip
     service_port = service_port
